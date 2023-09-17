f8a6c5a38bc312cf2b5a9c3d4b866dfd0c9d5295
AMNAc <amna.a.k.c@gmail.com>
2023-09-17 18:52:53 +0300

    last one with help

---
+++ index.js
@@ -0,0 +1,27 @@
+// Q1)Rewrite the following function as an arrow function:
+/******************************
+	function greet(name) {
+	  return `Hello, ${name}!`;
+	}
+********************************/
+const greet =(name)=>"hello", ${name}
+
+
+
+// Q2) Write a simple arrow function that takes two parameters and returns their sum.
+const sumroom=(x,y)=> x+y;
+   
+// Q3) Write a simple arrow function that squares a number.
+const squaresnumber =(num) => um*num;
+// Q4) [🌶️🌶️] Create an arrow function that takes an array of numbers and returns a new array with each number squared.
+
+const numbers=[2,3,4]:
+const squareArry =(arr) =>{
+    let newArray = [];
+    for (counter=0;counter<arr.length;counter++){
+        newArray.push( arr[counter]*arr[counter])
+    }
+retur newArray;
+
+
+    
